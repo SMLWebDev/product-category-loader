91772e0f306ba3243f29b528255338741113285c
Simon <simon@smlwebdevelopment.co.uk>
2025-05-23 16:58:24 +0100

    Adding Vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'smlwebdev/woo-category-grid-loader',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eeecca69fa947b3b2e20d7528b960fcd7bde72b2',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smlwebdev/woo-category-grid-loader' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eeecca69fa947b3b2e20d7528b960fcd7bde72b2',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
