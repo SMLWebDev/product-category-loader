39b997242e4eb07d5e362306b941b55b7ed3b6c5
Simon <simon@smlwebdevelopment.co.uk>
2025-06-03 13:30:18 +0100

    Seperated JS function into it's own file

--- src/Admin.php
+++ src/Admin.php
@@ -90,41 +90,6 @@ class Admin {
             <p><a href="https://github.com/SMLWebDev/woo-category-grid-loader/blob/main/ROADMAP.md" target="_blank">View plugin roadmap</a></p>
             <p>Plugin built and maintained by <a href="https://smlwebdevelopment.co.uk" target="_blank">SML Web Development</a>.</p>
         </div>
-
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                const inputs = document.querySelectorAll('.wcgl_generator input, .wcgl_generator select')
-                const output = document.getElementById('wcgl_shortcode')
-                const button = document.querySelector('.wcgl_shortcode_copy')
-                const generateButton = document.querySelector('.wcgl_generate')
-
-                const generateShortcode = () => {
-                    const perPage = document.getElementById('wcgl_per_page').value || 6
-                    const columns = document.getElementById('wcgl_columns').value || 3
-                    const order = document.getElementById('wcgl_order').value || 'ASC'
-                    const orderby = document.getElementById('wcgl_orderby').value || 'name'
-
-                    output.value = `[woo_category_grid per_page='${perPage}' columns='${columns}' orderby='${orderby}' order='${order}']`
-                };
-
-                inputs.forEach(input => {
-                    input.addEventListener('input', generateShortcode)
-                })
-
-                generateButton.addEventListener('click', generateShortcode)
-
-                button.addEventListener('click', function() {
-                    output.select()
-                    document.execCommand('copy')
-                    button.textContent = 'Copied!'
-                    setTimeout(() => {
-                        button.textContent = 'Copy Shortcode'
-                    }, 5000)
-                })
-
-                generateShortcode(); // Initial generation with default values
-            });
-        </script>
         <?php
     }
 }
